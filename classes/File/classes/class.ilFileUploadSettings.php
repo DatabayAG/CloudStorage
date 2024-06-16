3cb0c9266d60b9ec2beb616caea58178d004fe44
Shared FHD Development <eqsoft4@gmail.com>
2024-06-16 19:45:53 +0200

    uncomment strict types in buggy php scripts

--- classes/File/classes/class.ilFileUploadSettings.php
+++ classes/File/classes/class.ilFileUploadSettings.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+//declare(strict_types=1);
 
 /* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
 
--- classes/OwnCloud/classes/class.ilCloudStorageOwnCloud.php
+++ classes/OwnCloud/classes/class.ilCloudStorageOwnCloud.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+//declare(strict_types=1);
 
 // required??
 require_once(__DIR__ . "/../vendor/autoload.php");
