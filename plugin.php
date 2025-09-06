d91e87b84b9f81dadaa63892248b14690668c866
Stefan Schneider <eqsoft4@gmail.com>
2025-09-06 09:58:04 +0200

    bugfix plugin.php

--- plugin.php
+++ plugin.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -18,23 +17,3 @@ $ilias_max_version = "10.999";
 $responsible = "Stefan Schneider";
 $responsible_mail = "eqsoft4@gmail.com";
 ?>
-=======
-<?php
-
-declare(strict_types=1);
-
-// alphanumerical ID of the plugin; never change this
-$id = "xcls";
-
-// code version; must be changed for all code changes
-$version = "3.0.0";
-
-// ilias min and max version; must always reflect the versions that should
-// run with the plugin
-$ilias_min_version = "9.0";
-$ilias_max_version = "9.999";
-
-// optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Stefan Schneider";
-$responsible_mail = "eqsoft4@gmail.com";
->>>>>>> 3f2c673 (bump plugin version to 3.0.0 and fix line feeds to LF)
