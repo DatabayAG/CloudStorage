6a1941f4c95bc40b71727a5fd5b4c5a576b584f3
Stefan Schneider <eqsoft4@gmail.com>
2025-11-12 17:37:53 +0100

    bump version to 3.0.2

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 $id = "xcls";
 
 // code version; must be changed for all code changes
-$version = "3.0.1";
+$version = "3.0.2";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
